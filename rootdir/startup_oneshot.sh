c1c83d2863c31415b577a4f3691bd44ad7b4068b
brolerliew <brolerliew@gmail.com>
2023-05-15 17:47:09 +0800

    change dir permission when startup

--- rootdir/startup_oneshot.sh
+++ rootdir/startup_oneshot.sh
@@ -4,4 +4,13 @@ remount
 chmod 755 /system/bin
 chmod 755 /system/xbin
 chmod 6755 /system/xbin/su
-
+chmod 755 /product/bin
+chmod 775 /data
+chmod 775 /data/data
+chmod 755 /product/apex/*/bin
+chmod 755 /storage
+chmod 755 /system/apex/*/bin
+chmod 755 /system_ext/bin
+chmod 755 /system_ext/apex/*/bin
+chmod 755 /vendor/bin
+chmod 755 /vendor/apex/*/bin
